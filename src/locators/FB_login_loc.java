70d8ed43e68d336bd998d28ad7193f40cc6d9ab2
premdas4341 <37986766+premdas4341@users.noreply.github.com>
2018-10-27 21:30:15 -0400

    homework

--- src/locators/FB_login_loc.java
+++ src/locators/FB_login_loc.java
@@ -4,6 +4,6 @@ public class FB_login_loc {
 
 	public String emailOrPhone 	 		= "//input[@id='email']";
 	public String password 				= "//input[@id='pass']";
-	public String loginButton 			= "//input[@id='u_0_2']";
+	public String loginButton 			= "//input[@id='u_0_w']";
 	
 }
